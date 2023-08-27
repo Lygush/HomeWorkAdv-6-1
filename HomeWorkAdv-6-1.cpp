dcc0b98973b971c300f301a8ed4cebebe4c2b113
Ivan <kosarevsky2000@yandex.ru>
2023-08-27 20:16:35 +0300

    initial commit

---
+++ HomeWorkAdv-6-1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <set>
+#include <list>
+
+bool comp(std::pair<int, char> a, std::pair<int, char> b) {
+    return a.first > b.first;
+}
+
+int main() {
+    std::string line{"Hellow world!!"};
+    std::list<std::pair<int, char>> list;
+
+    std::multiset<char> set;
+    for (const auto& i: line) {
+        set.emplace(i);
+    }
+    for (const auto& i: set) {
+        list.push_back(std::make_pair(set.count(i),i));
+    }
+    list.sort(comp);
+    list.unique();
+    for (const auto& i: list) {
+        std::cout << i.second << ": ";
+        std::cout << i.first << std::endl;
+    }
+};
+
